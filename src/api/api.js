f58c710dd2fe3a66daf3c9254b2efb00d9d9adff
Sondre William Kaldhussæter-Knutsen <knutsensondre@gmail.com>
2021-05-27 01:08:11 +0200

    Added headers for cors

--- src/api/api.js
+++ src/api/api.js
@@ -2,16 +2,15 @@
 import axios from 'axios'
 
 const getData = async function(endpoint, query) {
-  //const headers = { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*"};
+  const headers = { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*"};
   const baseUrl = process.env.VUE_APP_API_URL;
   var url = `${baseUrl}/${endpoint}`;
-
+  
   if(query) {
     url = `${url}?${query}`;
   }
 
-  //const response = await fetch(url);
-  const response = await axios.get(url);
+  const response = await axios.get(url, {"headers": headers});
   return response;
 }
 
